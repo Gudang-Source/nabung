f3a5a35c4cb8f88b055d47e9669098a7a0700b28
silversteel <heatrigger19@gmail.com>
2016-12-11 13:15:12 +0700

    Modified dashboard page

--- page/dashboard.php
+++ page/dashboard.php
@@ -8,21 +8,5 @@
           </div>
         </div>
       </section>
-      <section class="content-box">
-        <div class="panel panel-success">
-          <div class="panel-heading">
-            <h3 class="panel-title">Saldo anda</h3>
-          </div>
-          <div class="panel-body">
-            <div class="huge">
-              Rp 1.000.000
-            </div>
-            <div class="progress progress-striped active">
-              <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="96" aria-valuemin="0" aria-valuemax="100" style="width:96%;">
-                <span class="sr-only">96% Tercapai</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+
     </main>
