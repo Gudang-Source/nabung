36b0f8ad7a2dcf2bded1c187224e31e84ce15f6a
Muhammad Faishal <heatrigger19@gmail.com>
2016-12-26 13:36:53 +0700

    Fix dashboard style

--- page/dashboard.php
+++ page/dashboard.php
@@ -8,10 +8,49 @@
           </div>
         </div>
       </section>
+      <section class="content-box">
+        <div class="row">
+          <div class="col-xs-12">
+            <h3 class="page-title">Dashboard</h3>
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <i class="fa fa-fw fa-user"></i>&nbsp;User Information
+                </h4>
+              </div>
+              <div class="panel-body">
+                <div>
+                  <div class="sub-title">
+                    <i class="fa fa-fw fa-dollar"></i>&nbsp;Saldo :
+                  </div>
+                  <p id="saldo" class="sub-value text-left"></p>
+                  <div class="clearfix"></div>
+                </div>
+                <div>
+                  <div class="sub-title">
+                    <i class="fa fa-fw fa-arrow-circle-down"></i>&nbsp;Pendapatan Bulan Ini :
+                  </div>
+                  <p id="incNow" class="sub-value text-left"></p>
+                  <div class="clearfix"></div>
+                </div>
+                <div>
+                  <div class="sub-title">
+                    <i class="fa fa-fw fa-arrow-circle-up"></i>&nbsp;Pengeluaran Bulan Ini :
+                  </div>
+                  <p id="outNow" class="sub-value text-left"></p>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 <!--
 TODO: Dasboard information
 -->
-<p id="saldo"></p>
-<p id="incNow"></p>
-<p id="outNow"></p>
     </main>
